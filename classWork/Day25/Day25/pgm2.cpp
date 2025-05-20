1e1ec4338a05d0d5e43c4754efe5cea78ba03813
NivyaPrakash <nivyaprakash2000@gmail.com>
2025-05-20 12:24:09 +0530

    added

--- classWork/Day25/Day25/pgm2.cpp
+++ classWork/Day25/Day25/pgm2.cpp
@@ -1,9 +1,8 @@
 #include<iostream>
-
+#include<cstring>
 using namespace std;
 
-void printDetails(EMP);
-void addEmployee(EMP,int);
+
 
 
 typedef struct Employee {
@@ -17,20 +16,21 @@ typedef struct Employee {
     int sal;
 } EMP;
 
-void printDetails(EMP e)
+void printDetails(EMP);
+void addEmployee(EMP, int);
+void deleteEmployee(EMP, int);
+void searchEmployee(EMP, int);
+void updateEmployee(EMP, int);
+
+
+void printDetails(EMP e[])
 {
     cout << "Enter employee details" << endl;
-    cout << e.empID << endl;
-    cout << e.empName << endl;
-    cout << e.D.day<< "\t"<<e.D.mnth<<"\t"<<e.D.year<<endl;
-    cout << e.sal << endl;
+    cout << e->empID << endl;
+    cout << e->empName << endl;
+    cout << e->D.day<< "\t"<<e->D.mnth<<"\t"<<e->D.year<<endl;
+    cout << e->sal << endl;
 }
-
-
-
-
-
-
 
 
 
@@ -42,6 +42,7 @@ void addEmployee(EMP e[], int& count)
     }
     cout << "Enter employee ID: ";
     cin >> e[count].empID;
+    cin.ignore();
     cout << "Enter the employee name:" << endl;
     cin.getline(e->empName, 20);
     cout << "Enter the join date" << endl;
@@ -52,5 +53,95 @@ void addEmployee(EMP e[], int& count)
     cout << "Employee added successfully.\n";
 }
 
+void deleteEmployee(EMP e[], int& count) {
+    int id;
+    cout << "Enter Product ID to delete: ";
+    cin >> id;
+    for (int i = 0; i < count; i++) {
+        if (e[i].empID == id) {
+            for (int j = i; j < count - 1; j++) {
+                e[j] = e[j + 1];
+            }
+            count--;
+            cout << "Product deleted successfully.\n";
+            return;
+        }
+    }
+    cout << "Employee not found.\n";
+}
 
+void searchEmployee(EMP e[], int count) {
+    int id;
+    cout << "Enter Product ID to search: ";
+    cin >> id;
+    for (int i = 0; i < count; i++) {
+        if (e[i].empID == id) {
+            cout << "Employee Found:\n";
+            cout << "ID: " << e[i].empID << endl;
+            cout << "Name: " << e[i].empName << endl;
+            cout << "Join date: " << e[i].D.day<<"\t"<< e[i].D.mnth <<"\t" << e[i].D.year << endl;
+            cout << "salary: " << e[i].sal << endl;
+            return;
+        }
+    }
+    cout << "Employee not found.\n";
+}
+
+
+void updateEmployee(EMP e[], int count) {
+    int id;
+    cout << "Enter employee ID to update: ";
+    cin >> id;
+    for (int i = 0; i < count; i++) {
+        if (e[i].empID == id) {
+            cout << "Enter new employee Name: ";
+          
+            cin.getline(e->empName, 20);
+            cout << "Enter new Quantity: ";
+            cin >> e[i].D.day>>e[i].D.mnth>>e[i].D.year;
+            cout << "Enter new salary: ";
+            cin >> e[i].sal;
+            cout << "Employee updated successfully.\n";
+            return;
+        }
+    }
+    cout << "employee not found.\n";
+}
+
+
+
+
+
+void menu()
+{
+    cout << "\nInventory Management System\n";
+    cout << "1. Add Employee\n2. delete Employee\n3. search Employee\n4. update Employee\n5. Display All\n6. Exit\n";
+}
+
+
+
+
+int main() {
+    EMP e[1000];
+    int count = 0;
+    int choice;
+    do {
+        menu();
+        cout << "Enter your choice: ";
+        cin >> choice;
+
+        switch (choice) {
+        case 1: addEmployee(e, count); break;
+        case 2: deleteEmployee(e, count); break;
+        case 3: searchEmployee(e, count); break;
+        case 4:updateEmployee(e, count);break;
+        case 5: printDetails(e); break;
+        case 6: cout << "Exiting...\n"; break;
+        default: cout << "Invalid choice. Try again.\n";
+
+        }
+    } while (choice != 6);
+
+    return 0;
+}
 
