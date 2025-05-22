243b3988a8d386cf7c17f7f6707fd4ca7cf3b8a4
NivyaPrakash <nivyaprakash2000@gmail.com>
2025-05-22 07:10:42 +0530

    added

---
+++ Assignment/day26/day26/MovieRating.cpp
@@ -0,0 +1,84 @@
+#include<iostream>
+using namespace std;
+
+class movieDet
+{
+private:
+	char title[20];
+	char genre[20];
+	float rating;
+public:
+	void setTitle(char*);
+	char* getTitle();
+	void setgenre(char*);
+	char* getgenre();
+	void setrating(int);
+	int getrating();
+	void getdetails();
+	void printrating();
+	void display();
+};
+
+void movieDet::setTitle(char* s)
+{
+	strcpy(title, s);
+}
+char* movieDet::getTitle() {
+	return title;
+}
+void movieDet::setgenre(char* g)
+{
+	strcpy(genre, g);
+}
+char* movieDet::getgenre()
+{
+	return genre;
+}
+void movieDet::setrating(int r)
+{
+	rating = r;
+}
+int movieDet::getrating()
+{
+	return rating;
+}
+
+
+void movieDet::getdetails()
+{
+	cout << "Enter the movie name:" << endl;
+	cin.getline(title, 20);
+	cout << "Enter the genre:" << endl;
+	cin.getline(genre, 20);
+	cout << "Enter the rating:" << endl;
+	cin >> rating;
+}
+
+void movieDet::printrating()
+{
+	if (rating >= 8)
+		cout << "Movie Hit" << endl;
+	else if (rating >= 4 && rating <= 8)
+		cout << "movie Average";
+	else
+		cout << "Movie Flop" << endl;
+}
+
+void movieDet::display()
+{
+	cout << "Movie Title:" << title << endl;
+	cout << "Movie genre:" << genre << endl;
+	cout << "Movie rating:" << rating << endl;
+	cout << "Movie review:";
+	printrating();
+}
+
+
+
+
+int main()
+{
+	movieDet m;
+	m.getdetails();
+	m.display();
+}
---
+++ Assignment/day26/day26/digitalclock.cpp
@@ -0,0 +1,89 @@
+#include<iostream>
+using namespace std;
+
+class digitalclock
+{
+private:
+	int hour;
+	int minute;
+	int second;
+public:
+	void setMinute(int);
+	int getMinute();
+	void setSecond(int);
+	int getSecond();
+	void setHour(int);
+	int getHour();
+	void display();
+	void digital();
+
+};
+
+
+void digitalclock::setMinute(int m) {
+	minute = m;
+}
+int digitalclock::getMinute() {
+	return minute;
+}
+
+void digitalclock::setSecond(int s) {
+	second = s;
+}
+int digitalclock::getSecond() {
+	return second;
+}
+
+
+void digitalclock::setHour(int h) {
+	hour = h;
+}
+int digitalclock::getHour() {
+	return hour;
+}
+
+
+
+void digitalclock::digital()
+{
+	second++;
+	if (second == 60)
+	{
+		second = 0;
+		minute++;
+	}
+	if (minute == 60)
+	{
+		minute = 0;
+		hour++;
+	}
+	if (hour == 60)
+	{
+		hour = 0;
+	}
+
+}
+
+
+void digitalclock::display()
+{
+	cout << "minute:" << minute << endl; ;
+	cout << "hour:" << hour<<endl;
+	cout << "second:" << second<<endl;
+}
+
+
+
+int main()
+{
+	int m = 23;
+	int h = 50;
+	int s = 40;
+
+	digitalclock c;
+	c.setSecond(s);
+	c.setMinute(m);
+	c.setHour(h);
+	c.digital();
+	c.display();
+}
