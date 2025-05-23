b5aabb6c9bfc813a6d28396df7131655fb755385
NivyaPrakash <nivyaprakash2000@gmail.com>
2025-05-23 12:25:30 +0530

    added

---
+++ assessments/week06/week06/pgm1.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+using namespace std;
+
+
+class Flight
+{
+private:
+	string flightNo;
+	int totalseat;
+	int bookseat;
+public:
+	Flight(string no,int t)
+	{
+		flightNo = no;
+		totalseat = t;
+		
+	}
+	
+	void bookSeats(int b)
+	{
+		bookseat = b;
+	}
+	
+
+	void showAvailability()
+	{
+		int av = totalseat-bookseat;
+		if (bookseat >= av)
+		{
+			cout << "Booking Failed: Not enough seats.";
+		}
+		else
+		{
+			cout << "Flight:" << flightNo << "|" << "Seats Available : " << av;
+		}
+	}
+
+};
+
+int main()
+{
+
+	Flight f1("AI203", 100);
+	f1.bookSeats(80);
+	f1.showAvailability();
+}
